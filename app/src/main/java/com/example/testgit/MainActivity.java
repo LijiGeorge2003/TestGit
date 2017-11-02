c9eb0e23586629e197735ddfb25102fd7b10f263
Liji George <liji.george2003@gmail.com>
2017-11-02 19:03:47 +0530

    second commit

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this,"Hello World", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
     }
 }
